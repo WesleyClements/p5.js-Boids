b205dcb605c2e2e3e36db69ce034001b850c68d7
Lila Richardson <lilarich@ad.unc.edu>
2020-02-26 19:47:12 -0500

    added board and window resizing

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,5 +1,28 @@
+let rowCount: number = 8;
+let colCount: number = 9;
+let board: string[][];
+
+
 function setup() {
+    board = [];
+    for (let row = 0; row < rowCount; row++) {
+        board[row] = [];
+        for (let col = 0; col < colCount; col++) {
+            board[row][col] = " ";
+        } 
+    }
+    board[3][2] = "x";
+    console.log(board);
+
+    createCanvas(windowWidth, windowHeight);
+}
+function windowResized() {
+   resizeCanvas(windowWidth, windowHeight); 
 }
 
 function draw() {
+    textSize(32);
+    text("peepeepoopoo", 500, 100);
 }
+
+
