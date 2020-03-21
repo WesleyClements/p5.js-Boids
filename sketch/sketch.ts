607d04c30a2cdb22e8940a80f42ed18f5e74b004
WesleyClements <wesley.clements@gmail.com>
2020-03-20 19:45:03 -0700

    updates to use modules and es6

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,28 +1,20 @@
-let rowCount: number = 8;
-let colCount: number = 9;
-let board: string[][];
-
-
-function setup() {
-    board = [];
-    for (let row = 0; row < rowCount; row++) {
-        board[row] = [];
-        for (let col = 0; col < colCount; col++) {
-            board[row][col] = " ";
-        } 
-    }
-    board[3][2] = "x";
-    console.log(board);
-
-    createCanvas(windowWidth, windowHeight);
+declare global {
+  interface Window {
+    setup: () => void;
+    windowResized: () => void;
+    keyPressed: () => void;
+    mousePressed: () => void;
+    draw: () => void;
+  }
 }
+
+window.setup = () => {
+  createCanvas(windowWidth, windowHeight);
+};
 function windowResized() {
-   resizeCanvas(windowWidth, windowHeight); 
+  resizeCanvas(windowWidth, windowHeight);
 }
 
-function draw() {
-    textSize(32);
-    text("peepeepoopoo", 500, 100);
-}
+function draw() {}
 
-
+export default null;
